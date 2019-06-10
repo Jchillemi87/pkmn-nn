791dd54c0630b5a2fbe912cec2ff39f282950b0a
jchillemi87 <jchillemi87@gmail.com>
2019-06-10 06:17:34 -0400

    Updated model.js, still needs work

--- model.js
+++ model.js
@@ -6,6 +6,8 @@ util.inspect.defaultOptions.colors = true;
 const fs = require('fs');
 const readFile = util.promisify(fs.readFile);
 
+const clonedeep = require('lodash.clonedeep')
+
 const { sulcalc } = require('./sulcalc.js');
 const { Pokemon } = require('./sulcalc.js');
 const { Move } = require('./sulcalc.js');
@@ -17,103 +19,218 @@ const { getLogLocal } = require('./logParser.js');
 
 const { BattlePokedex } = require('./Pokemon-Showdown/data/pokedex.js');
 const { BattleMovedex } = require('./Pokemon-Showdown/data/moves.js');
+const { BattleItems } = require('./Pokemon-Showdown/data/items.js');
+
 const tools = require('./Tools.js');
 
 const { RB_set } = require('./RBPI.js');
 
-function status2Binary(status) {
-  switch (status) {
-    case "brn":
-      return [0, 0, 0, 0, 0, 1];
-    case "frz":
-      return [0, 0, 0, 0, 1, 0];
-    case "par":
-      return [0, 0, 0, 1, 0, 0];
-    case "psn":
-      return [0, 0, 1, 0, 0, 0];
-    case "slp":
-      return [0, 1, 0, 0, 0, 0];
-    case "tox":
-      return [1, 0, 0, 0, 0, 0];
-    default:
-      return [0, 0, 0, 0, 0, 0];
-  }
-}
+class Model {
+  constructor(json) {
+    this.game = json;
+    this.turns = JSON.parse(this.game);
+    this.winner = {
+      player: getWinner(this.turns[this.turns.length - 1]),
+      pokemon: [],
+      active: {}
+    }
+    this.foe = {
+      player: this.winner.player == 'p1' ? 'p2' : 'p1'
+    };
 
-function status2Binary(status) {
-  switch (status) {
-    case "brn":
-      return [0, 0, 0, 0, 0, 1];
-    case "frz":
-      return [0, 0, 0, 0, 1, 0];
-    case "par":
-      return [0, 0, 0, 1, 0, 0];
-    case "psn":
-      return [0, 0, 1, 0, 0, 0];
-    case "slp":
-      return [0, 1, 0, 0, 0, 0];
-    case "tox":
-      return [1, 0, 0, 0, 0, 0];
-    default:
-      return [0, 0, 0, 0, 0, 0];
+    this.init();
   }
-}
 
-function volatile2Binary(condition) {
-  switch (condition) {
-    case "Substitute":
+  async init() {
+    //retreave all known data from the last turn and let the winner know his team.
+    //this is obsolete
+    this.turns[this.turns.length - 1][this.winner.player].pokemon.forEach(pkmn => {
+      getPKMNInfo(pkmn);
+      pkmn.curHP = 100;
+      this.winner.pokemon.push(pkmn);
+    });
+
+    //  console.log(util.inspect(this.winner));
+
+    for await (const [turnNum, turn] of this.turns.entries()) {
+      if (turnNum == 0 || turnNum == this.turns.length - 1) {
+        continue;
+      }
+      await this.getState(turn);
+      //        console.log(util.inspect(this, { depth: 3 }));
+      console.log(await this.normalize());
+      console.log('+++++++++++++++++++++++++++++++++++');
+    }
+  }
+
+  async getState(turn) {
+    this.active = turn[this.winner.player].pokemon.find(pkmn => {
+      return pkmn.isActive == true;
+    });
+
+    this.foe = { ...turn[this.foe.player], ...this.foe };
+
+    this.foe.active = turn[this.foe.player].pokemon.find(pkmn => {
+      return pkmn.isActive == true;
+    });
+
+    this.active = await getPKMNInfo(this.active);
+    this.foe.active = await getPKMNInfo(this.foe.active);
+
+    this.pseudoWeather = turn.pseudoWeather || {};
+    this.terrain = turn.terrain || [];
+    this.weather = turn.weather || [];
+
+    for (let x = turn[this.foe.player].teamsize - turn[this.foe.player].pokemon.length; x > 0; x--) {
+      turn[this.foe.player].pokemon.push({
+        curHP: undefined,
+        fainted: false
+      });
+    }
+
+    this.winner.remaining = this.getRemaining(turn[this.winner.player].pokemon);
+    this.foe.remaining = this.getRemaining(turn[this.foe.player].pokemon);
+
+    if (!this.foe.remaining.length) {
       return;
-    case "confusion":
-      return (2).toString(2);
+    }
 
-    case "":
-    case "":
-    case "":
+    try {
+      this.simplify();
+    } catch (error) {
+      console.log(turn.turn);
+      console.error(error);
+    }
 
-    default:
+    return new Promise(resolve => resolve());
+  }
+
+  simplify(turn) {
+    this.moveFirst = tools.getFinalSpeed(this.active || 'none', this.weather[0] || 'none') > tools.getFinalSpeed(this.foe.active || 'none', this.weather[0] || 'none'); //Check if winner moves 1st next turn
+  }
+
+  getRemaining(team) {
+    return team.filter(pkmn => {
+      return pkmn.fainted == false;
+    });
+  }
+
+  getNewMovesDmgs(atk, def, MOVES) {
+    let temp;
+    let moves = [];
+    let newMove = { name: "", dmgs: [] };
+    let atkr = {};
+    let defr = {};
+
+    for (var prop in atk) atkr[prop] = atk[prop];
+    for (var prop in def) defr[prop] = def[prop];
+
+    atkr.boosts = Object.values(atk.boosts);
+    defr.boosts = Object.values(def.boosts);
+    atkr.boosts.unshift(0);
+    defr.boosts.unshift(0);
+
+    MOVES.forEach((x, n) => {
+      if (BattleMovedex[tools.getId(MOVES[n])].basePower) {
+        newMove = { name: x, dmgs: [] };
+        temp = sulcalc(atkr, defr, { name: MOVES[n] }).damage._data;
+        temp.forEach((x, y) => {
+          for (let X = 0; X < x.value; X++) {
+            newMove.dmgs.push(y);
+          }
+          //    console.log(y+'='+ Math.ceil((this.foe.active.curHP/100) / (y / this.foe.active.maxHP)) + " * "+x.value)
+        });
+        newMove.avgDmg =
+          newMove.dmgs.reduce((a, b) => a + b, 0) / newMove.dmgs.length;
+        moves.push(newMove);
+      }
+    });
+    return Promise.resolve(moves);
+  }
+
+  async normalize() {
+    //two variables are used to make debugging easier. The values in data are labeled using an object.
+    let data = {};
+    let returnValue = [];
+
+    data.field = (field2Binary(this) || new Array(6).fill(0));
+    data.terrain = terrain2Binary(this) || new Array(4).fill(0);
+    data.weather = weather2Binary(this) || new Array(4).fill(0);
+
+    data.moveFirst = this.moveFirst ? 1 : 0;
+    data.foeHP = this.foe.active.curHP == -1 ? 1 : this.foe.active.curHP / this.foe.active.maxHP;
+    data.foeStatus = status2Binary(this.foe.active.status || new Array(6).fill(0));
+    data.foeBoosts = boosts2Binary(this.foe.active.boosts || new Array(7).fill(0));
+    data.foeVolatile = volatile2Binary(this.foe.active.volatiles || new Array(14).fill(0));
+    data.foeSideCond = sideConditions2Binary(this.foe.sideConditions || new Array(4).fill(0));
+
+    data.foeRemaining = this.foe.remaining.length / 6;
+
+    let moveList = this.foe.active.moves;
+
+    if (this.foe.active.moves.length < 4) {
+      moveList = this.foe.active.RBData.probModel.moves.reduce(
+        (moves, move) => {
+          moves.push(move.move);
+          return moves;
+        },
+        []
+      );
+    }
+
+    this.active.movesDmgs = await this.getNewMovesDmgs(
+      this.active,
+      this.foe.active,
+      this.active.moves
+    );
+
+    this.foe.active.movesDmgs = await this.getNewMovesDmgs(
+      this.foe.active,
+      this.active,
+      this.foe.active.moves
+    );
+
+    data.foeBestDmg = Math.min(
+      ((getBestDmg(this.foe.active.movesDmgs).avgDmg / this.active.maxHP) * 100) /
+      this.active.curHP) || 0;
+
+    data.bestDmg = Math.min( //movesDmgs 
+      ((getBestDmg(this.active.movesDmgs).avgDmg / this.foe.active.maxHP) * 100) /
+      this.active.curHP) || 0;
+
+    data.curHP = this.active.curHP == -1 ? 1 : this.active.curHP / this.active.maxHP;
+    data.status = status2Binary(this.active.status || new Array(6).fill(0));
+    data.boosts = boosts2Binary(this.active.boosts || new Array(7).fill(0));
+    data.volatile = volatile2Binary(this.active.volatiles || new Array(14).fill(0));
+    data.sideCond = sideConditions2Binary(this.winner.sideConditions || new Array(4).fill(0));
+
+    data.remaining = this.winner.remaining.length / 6;
+
+    returnValue = [data.field, data.terrain, data.weather, data.moveFirst, data.foeHP, data.foeStatus, data.foeBoosts, data.foeVolatile, data.foeSideCond, data.foeRemaining, data.foeBestDmg, data.bestDmg, data.curHP, data.status, data.boosts, data.volatile, data.sideCond, data.remaining];
+    return new Promise((resolve) => resolve(returnValue));
   }
 }
 
 async function main(json) {
-  let temp = await getLogLocal(json);
-  this.turns = await JSON.parse(temp);
-  this.winner = await getWinner(this.turns[this.turns.length - 1]);
+  let file = await getLogLocal(json);
+  let model = new Model(file);
+}
 
-  this.currentTurn = 0;
-  this.foe =
-    this.turns[currentTurn].p1 == this.winner
-      ? this.turns[currentTurn].p2
-      : this.turns[currentTurn].p1;
-  this.active = this.winner.pokemon.find(pkmn => {
-    return pkmn.isActive == true;
+//main("./replays.json/gen7randombattle-843768871.json");
+//main("./replays.json/gen7randombattle-826319449.json");
+main('./replays.json/gen7randombattle-832046044.json');
+
+function getBestDmg(moves) {
+  if (!moves.length) return 0;
+  return moves.reduce((maxDmg, dmg) => {
+    return (maxDmg.avgDmg < dmg.avgDmg) | 0 ? dmg : maxDmg;
   });
-  this.foe.active = await getPKMNInfo(
-    this.foe.pokemon.find(pkmn => {
-      return pkmn.isActive == true;
-    })
-  );
-
-  //console.log(this.foe.active);
-
-  for (let x = this.foe.teamsize - this.foe.pokemon.length; x > 0; x--) {
-    this.foe.pokemon.push({
-      curHP: 100,
-      fainted: false
-    });
-  }
-
-  this.model = new Model(this);
 }
 
 function getWinner(turn) {
-  let winner = turn.winner;
-  winner = turn[winner];
-  //console.log(winner);
-  winner.pokemon.forEach(pkmn => {
-    getPKMNInfo(pkmn);
-    pkmn.curHP = 100;
-  });
-  return winner;
+  let winningPlyr = turn.winner;
+  winner = turn[winningPlyr];
+  return winningPlyr;
 }
 
 async function getPKMNInfo(pkmn) {
@@ -136,11 +253,11 @@ async function getPKMNInfo(pkmn) {
   });
 
   pkmn = await RBTidy(pkmn);
-  pkmn.fainted = false;
+  pkmn.fainted = pkmn.fainted ? true : false;
   pkmn.lastMove = null;
-  delete pkmn.boosts;
+  //  delete pkmn.boosts;
   delete pkmn.status;
-  return pkmn;
+  return new Promise(resolve => resolve(pkmn));
 }
 
 function RBTidy(pkmn) {
@@ -171,8 +288,6 @@ function RBTidy(pkmn) {
   return pkmn;
 }
 
-main("./replays.json/gen7randombattle-603536613.json");
-
 function combinedData(p1, p2) {
   p2.moves.forEach((x, n) => {
     let moveName = BattleMovedex[x].name;
@@ -180,119 +295,6 @@ function combinedData(p1, p2) {
   });
   if (!p1.ability && p2.ability) p1.ability = p2.ability;
   if (!p1.item && p2.item) p1.item = p2.item;
-}
-
-class Model {
-  constructor(state) {
-    this.winner = state.winner;
-    this.foe = state.foe;
-    this.active = state.active;
-    this.moveFirst =
-      tools.getFinalSpeed(state.active) > tools.getFinalSpeed(state.foe.active); //Check if winner moves 1st next turn
-    this.winner.remaining = this.getRemaining(this.winner);
-    this.foe.remaining = this.getRemaining(this.foe);
-
-    this.active.moves = this.getNewMovesDmgs(
-      this.active,
-      this.foe.active,
-      this.active.moves
-    );
-
-    this.foe.active.moves = this.getNewMovesDmgs(
-      this.foe.active,
-      this.active,
-      this.foe.active.moves
-    );
-
-    console.log(this.normalize());
-  }
-
-  getRemaining(plyr) {
-    return plyr.pokemon.filter(pkmn => {
-      return pkmn.fainted == false;
-    });
-  }
-
-  getNewMovesDmgs(atk, def, MOVES) {
-    let dmgs = new Map();
-    let temp;
-    let dmgsPerc = [];
-    let moves = [];
-    let newMove = { name: "", dmgs: [] };
-    MOVES.forEach((x, n) => {
-      if (BattleMovedex[tools.getId(MOVES[n])].basePower) {
-        newMove = { name: x, dmgs: [] };
-        temp = sulcalc(atk, def, { name: MOVES[n] }).damage._data;
-        temp.forEach((x, y) => {
-          for (let X = 0; X < x.value; X++) {
-            newMove.dmgs.push(y);
-          }
-          //    console.log(y+'='+ Math.ceil((this.foe.active.curHP/100) / (y / this.foe.active.maxHP)) + " * "+x.value)
-        });
-        newMove.avgDmg =
-          newMove.dmgs.reduce((a, b) => a + b, 0) / newMove.dmgs.length;
-        moves.push(newMove);
-      }
-    });
-    return moves;
-  }
-
-  normalize() {
-    let data = [];
-    data.push(this.moveFirst ? 1 : 0);
-    data.push(this.foe.active.curHP / 100);
-    data.push(status2Binary(this.foe.active.status));
-    data.push(this.foe.remaining.length / 6);
-
-    let moveList = this.foe.active.moves;
-
-    if (this.foe.active.moves.length < 4) {
-      moveList = this.foe.active.RBData.probModel.moves.reduce(
-        (moves, move) => {
-          moves.push(move.move);
-          return moves;
-        },
-        []
-      );
-    }
-
-    moveList = this.getNewMovesDmgs(this.foe.active, this.active, moveList);
-
-    data.push(
-      ((getBestDmg(moveList).avgDmg / this.active.maxHP) * 100) /
-      this.active.curHP
-    );
-
-    /*best accurate and dmg attack here*/
-    /*foe item data? or maybe passive heal or dmg or if move locked*/
-
-    /*our best stab attack here*
-    /*our item data? or maybe passive heal or dmg or if move locked*/
-
-    data.push(
-      ((getBestDmg(this.active.moves).avgDmg / this.active.maxHP) * 100) /
-      this.active.curHP
-    );
-
-    data.push(this.active.curHP / 100);
-    data.push(status2Binary(this.active.status));
-    data.push(this.winner.remaining.length / 6);
-
-    /*
-
-Their Volitile Status
-Our Volitile Status
-*/
-    /*field data here maybe?*/
-
-    return flattenDeep(data);
-  }
-}
-
-function getBestDmg(moves) {
-  return moves.reduce((maxDmg, dmg) => {
-    return (maxDmg < dmg.avgDmg) | 0 ? dmg : maxDmg;
-  });
 }
 
 function isMega(pkmn) {
@@ -304,10 +306,6 @@ function isMega(pkmn) {
 }
 
 /*********
-+who goes first
-+foe curHP %
-+foe status
-+foe remaining pkmn
 foe best dmg attack
 foe item
 
@@ -315,17 +313,195 @@ our best dmg
 our best dmg stab attack
 
 our item
-our curHP %
-our status
-our remaining pkmn
-
-our and foe's volatile state (taunted)
-field data
-
-Foe Volatile Status
-Our Volatile Status
-
 */
+
+function status2Binary(status) {
+  switch (status) {
+    case "brn":
+      return [0, 0, 0, 0, 0, 1];
+    case "frz":
+      return [0, 0, 0, 0, 1, 0];
+    case "par":
+      return [0, 0, 0, 1, 0, 0];
+    case "psn":
+      return [0, 0, 1, 0, 0, 0];
+    case "slp":
+      return [0, 1, 0, 0, 0, 0];
+    case "tox":
+      return [1, 0, 0, 0, 0, 0];
+    default:
+      return new Array(6).fill(0);
+  }
+}
+
+function sideConditions2Binary(side) {
+  let sideCon = [0, 0, 0, 0];
+
+  side.forEach((x, n) => {
+    switch (x) {
+      case "move: Stealth Rock":
+        sideCon[0] += 1;
+        break;
+      case "Spikes":
+        sideCon[1] += 1;
+        break;
+      case "move: Toxic Spikes":
+        sideCon[2] += 1;
+        break;
+      case "move: Sticky Web":
+        sideCon[3] += 1;
+        break;
+    }
+  })
+
+  sideCon[1] /= 3;
+  sideCon[2] /= 2;
+
+  return sideCon;
+
+}
+/*
+pursuit dmg while switching
+sideCondition - healingwish, lunardance, wish ???? 
+-sidestart - move: Light Screen, move: Lucky Chant, Mist, Reflect, move: Safeguard
+move: Stealth Rock, move: Sticky Web, move: Tailwind, move: Toxic Spikes, 
+
+-start (typechange, Doom Desire, typeadd, move: Future Sight, move: Throat Chop)
+-activate (move: Guard Split ????, move: Lock-On)
+
+partiallytrapped - Rapid Spin or Substitute, or user switched
+lockedmove
+stockpile - the -end is Stockpile //useful info: pokemon.volatiles['stockpile'].layers  //not used?
+*/
+
+function boosts2Binary(boosts) {
+  var normalized = [];
+  for (var boost of Object.values(boosts)) {
+    normalized.push((boost + 6) / 12);
+  }
+  return normalized;
+}
+
+function volatile2Binary(condition) {
+  switch (condition) {
+    case "Substitute":
+      return (Math.pow(2, 1)).toString(2); //replace later maybe with the remaining HP of the sub
+
+    case "confusion":
+      return (Math.pow(2, 2)).toString(2);
+
+    case "move: Leech Seed":
+      return (Math.pow(2, 3)).toString(2);
+
+    case "move: Focus Energy ":
+      return (Math.pow(2, 4)).toString(2);
+
+    case "Magnet Rise":
+      return (Math.pow(2, 5)).toString(2);
+
+    case "Smack Down":
+      return (Math.pow(2, 6)).toString(2);
+
+    case "move: Taunt":
+      return (Math.pow(2, 7)).toString(2);
+
+    case "move: Yawn":
+      return (Math.pow(2, 8)).toString(2);
+
+    case "Attract":
+      return (Math.pow(2, 9)).toString(2);
+
+    case "perish3":
+      return (Math.pow(2, 10)).toString(2);
+
+    case "Encore":
+      return (Math.pow(2, 11)).toString(2);
+
+    case "Autotomize":
+      return (Math.pow(2, 12)).toString(2);
+
+    case "mustrecharge": //-mustrecharge slaking
+      return (Math.pow(2, 13)).toString(2);
+
+    case "trapped": //-activate
+      return (Math.pow(2, 14)).toString(2);
+
+    default:
+      return new Array(14).fill(0);
+  }
+}
+
+function field2Binary(battle) {
+  let fieldCon = [0, 0, 0, 0, 0, 0];
+
+  if (!battle.pseudoWeather) return fieldCon;
+  Object.keys(battle.pseudoWeather).forEach((x, n) => {
+    switch (x) {
+      case "move: Trick Room":
+        fieldCon[0] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+      case "move: Water Sport":
+        fieldCon[1] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+      case "move: Wonder Room":
+        fieldCon[2] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+      case "move: Gravity":
+        fieldCon[3] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+      case "move: Magic Room":
+        fieldCon[4] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+      case "move: Mud Sport":
+        fieldCon[5] = 1 - ((battle.currentTurn - battle.pseudoWeather[x][1]) + 1) / 5;
+        break;
+    }
+  });
+
+  return fieldCon;
+}
+
+function terrain2Binary(battle) {
+  let terrain = [0, 0, 0, 0];
+  if (!battle.terrain) return terrain;
+  switch (battle.terrain[0]) {
+    case "move: Psychic Terrain":
+      terrain[0] = 1 - ((battle.currentTurn - battle.terrain[1]) + 1) / 5;
+      break;
+    case "move: Grassy Terrain":
+      terrain[1] = 1 - ((battle.currentTurn - battle.terrain[1]) + 1) / 5;
+      break;
+    case "move: Misty Terrain":
+      terrain[2] = 1 - ((battle.currentTurn - battle.terrain[1]) + 1) / 5;
+      break;
+    case "move: Electric Terrain":
+      terrain[3] = 1 - ((battle.currentTurn - battle.terrain[1]) + 1) / 5;
+      break;
+  }
+
+  return terrain;
+}
+
+function weather2Binary(battle) {
+  let weather = [0, 0, 0, 0];
+  if (!battle.weather) return weather;
+  switch (battle.weather[0]) {
+    case "RainDance":
+      weather[0] = 1 - ((battle.currentTurn - battle.weather[1]) + 1) / 8;
+      break;
+    case "SunnyDay":
+      weather[1] = 1 - ((battle.currentTurn - battle.weather[1]) + 1) / 8;
+      break;
+    case "SandStorm":
+      weather[2] = 1 - ((battle.currentTurn - battle.weather[1]) + 1) / 8;
+      break;
+    case "Hail":
+      weather[3] = 1 - ((battle.currentTurn - battle.weather[1]) + 1) / 8;
+      break;
+  }
+
+  return weather;
+}
 
 function flattenDeep(arr1) {
   return arr1.reduce(
