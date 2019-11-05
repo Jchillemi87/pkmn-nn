0336f89a497922305ed5ec69f8fe6d372340b745
jchillemi87 <jchillemi87@gmail.com>
2019-11-04 23:38:00 -0500

    cleaner replayDL.js

--- replayDL.js
+++ replayDL.js
@@ -5,9 +5,13 @@ util.inspect.defaultOptions.depth = Infinity;
 util.inspect.defaultOptions.colors = true;
 
 const readDir = util.promisify(fs.readdir);
+const Read = util.promisify(fs.readFile);
+const Move = util.promisify(fs.rename);
 
 const replaysPath = './replays/logs/';
 const replaysErrors = './replays/errors/';
+const inactivity = './replays/inactivity/';
+const forfeited = './replays/forfeited/';
 const REGEX = /(?<=\<a\shref=\"\/).*\d(?=\")/g;
 const url = 'https://replay.pokemonshowdown.com/search?user=&format=gen7randombattle&rating&output=html&page='; //25
 //var url = 'https://replay.pokemonshowdown.com/search?user=&format=gen7randombattle&rating&page=25&output=html';
@@ -21,24 +25,68 @@ async function getLog(url) {
     }
 }
 
+async function findReplays(location, str) {
+    try {
+        let files = await readDir(location, 'utf8');
+        let replays = [];
+        for await (file of files) {
+            let log = await Read(location + file);
+            if (log.includes(str)) {
+                replays.push({ location: location, file: file })
+            }
+        }
+        return replays;
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+async function moveReplays(logFiles, dest) {
+    try {
+        for (log of logFiles) {
+            await Move(log.location + log.file, dest + log.file);
+            console.log(`${file} moved to ${dest}`);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 async function main() {
-    const localReplays = await readDir(replaysPath, 'utf8');
+    var localReplays = await readDir(replaysPath, 'utf8');
+    let excludeReplays = [];
     const localReplaysErrors = await readDir(replaysErrors, 'utf8');
+    const localReplaysForfeited = await readDir(forfeited, 'utf8');
+    const localReplaysInactivity = await readDir(inactivity, 'utf8');
+
+    excludeReplays.push(...localReplaysErrors, ...localReplaysForfeited, ...localReplaysInactivity);
 
     let total = 0;
+    console.log(`URL: ${url}`);
     for (let i = 1; i <= 25; i++) {
+        console.log(`Page: ${i}`);
         let res = await getLog(url + i);
         res = res.match(REGEX);
         res.forEach(async (x, n) => {
-            if (localReplays.includes(x + '.log') || localReplaysErrors.includes(x + '.log') ) return;
+            if (localReplays.includes(x + '.log') || excludeReplays.includes(x + '.log')) return;
             let response = await getLog('https://replay.pokemonshowdown.com/' + x + '.log');
             fs.writeFile(replaysPath + x + '.log', response, () => {
                 total++;
-                console.log("wrote " + x + ".log to FS.");
+                //console.log("wrote " + x + ".log to FS.");
             });
         });
     }
+
     console.log(`Downloaded ${total} new replays`);
+
+    let forfeitedReplays = await findReplays(replaysPath, 'forfeited');
+    await moveReplays(forfeitedReplays, forfeited);
+
+    let inactivityReplays = await findReplays(replaysPath, 'inactivity');
+    await moveReplays(inactivityReplays, inactivity);
+
+    console.log("Done");
 }
 
 main();
